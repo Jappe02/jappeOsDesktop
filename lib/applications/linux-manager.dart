371fd38075b577eb361af0d251dfee206c0420f2
Jappe02 <jasper.mannonen@gmail.com>
2020-08-30 13:24:13 +0300

    added linux manager.dart

---
+++ lib/applications/linux-manager.dart
@@ -0,0 +1,45 @@
+import 'package:flutter/material.dart';
+import 'package:simpaDesktop/components/system.dart';
+import 'dart:io';
+
+void main() {
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Settings',
+      theme: ThemeData(
+        primarySwatch: Colors.red,
+        visualDensity: VisualDensity.adaptivePlatformDensity,
+      ),
+      home: MyHomePage(title: 'Linux Manager'),
+    );
+  }
+}
+
+class MyHomePage extends StatefulWidget {
+  MyHomePage({Key key, this.title}) : super(key: key);
+
+  final String title;
+
+  @override
+  _MyHomePageState createState() => _MyHomePageState();
+}
+
+class _MyHomePageState extends State<MyHomePage> {
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text(widget.title),
+      ),
+      body: Container(
+        
+      ),
+    );
+  }
+}

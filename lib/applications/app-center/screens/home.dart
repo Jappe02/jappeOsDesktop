7d7c2dfa46ce17a999634017eacdd6d8bd718c28
Jappe02 <jasper.mannonen@gmail.com>
2020-10-05 17:43:32 +0300

    Customized the app bar

--- lib/applications/app-center/screens/home.dart
+++ lib/applications/app-center/screens/home.dart
@@ -36,40 +36,135 @@ class _HomeScreenState extends State<HomeScreen> {
   Widget build(BuildContext context) {
     return Scaffold(
       body: Container(
-        child: Positioned(
-          left: 0,
-          top: 0,
-          right: 0,
-          child: Container( // custom appbar
-            
-            color: Colors.green,
-            child: Align(
-              alignment: Alignment.topRight,
-              child: Row(
-                children: <Widget>[
-                  Container(
-                    width: 50,
-                    child: FlatButton(
-                      padding: EdgeInsets.all(0),
-                      onPressed: () {
+        child: Container( // Custom appBar
+          height: 50,
+          color: Colors.green,
+          child: Stack(
+            children: [
+              Positioned(
+                left: 10,
+                child: Row(
+                  children: [
+                    Container(
+                      width: 40,
+                      child: FlatButton(
+                        shape: RoundedRectangleBorder(
+                          borderRadius: BorderRadius.circular(30)
+                        ),
+                        padding: EdgeInsets.all(0),
+                        onPressed: () {
 
-                      },
-                      child: Icon(Icons.menu, color: Colors.white,),
-                    ),
-                  ),
-                  Container(width: 10,),
-                  Container(
-                    child: Text(
-                      'App center',
-                      style: TextStyle(
-                        fontSize: 20,
-                        color: Colors.white,
+                        },
+                        child: Icon(Icons.menu, color: Colors.white,),
                       ),
                     ),
-                  ),
-                ],
+                    Container(width: 10,),
+                    Container(
+                      child: Text(
+                        'App center',
+                        style: TextStyle(
+                          fontSize: 20,
+                          color: Colors.white,
+                        ),
+                      ),
+                    ),
+                  ],
+                ),
+                
               ),
-            ),
+              Positioned(
+                top: 10,
+                left: 200,
+                right: 200,
+                height: 30,
+                child: Container(
+                  child: FlatButton(
+                    hoverColor: Colors.black.withOpacity(0.0),
+                    onPressed: () {},
+                    shape: RoundedRectangleBorder(
+                      borderRadius: BorderRadius.circular(15)
+                    ),
+                    child: Stack(
+                      children: [
+                        Positioned(
+                          left: 0,
+                          height: 30,
+                          width: 30,
+                          child: Icon(Icons.search, color: Colors.white,),
+                        ),
+                        Positioned(
+                          top: 5,
+                          left: 60,
+                          right: 40,
+                          height: 30,
+                          child: Text(
+                            'Search for apps, games, extensions, and more!',
+                            style: TextStyle(
+                              color: Colors.white54,
+                              fontSize: 17,
+                            ),
+                          ),
+                        ),
+                        Positioned(
+                          right: 0,
+                          height: 30,
+                          width: 30,
+                          child: Icon(Icons.keyboard_arrow_down, color: Colors.white,),
+                        ),
+                      ],
+                    ),
+                  ),
+                  decoration: BoxDecoration(
+                    borderRadius: BorderRadius.circular(15),
+                    color: Colors.green[800],
+                  ),
+                ),
+              ),
+              Positioned(
+                right: 10,
+                child: Row(
+                  children: [
+                    Container(
+                      width: 180,
+                      child: FlatButton(
+                        shape: RoundedRectangleBorder(
+                          borderRadius: BorderRadius.circular(30)
+                        ),
+                        padding: EdgeInsets.all(0),
+                        onPressed: () {
+
+                        },
+                        child: Container(
+                          width: 180,
+                          height: 35,
+                          child: Stack(
+                            children: [
+                              Positioned(
+                                width: 40,
+                                right: 0,
+                                child: Icon(Icons.account_circle, color: Colors.white, size: 35,),
+                              ),
+                              Positioned(
+                                left: 10,
+                                top: 7,
+                                right: 45,
+                                child: Text(
+                                  'Login',
+                                  style: TextStyle(
+                                    color: Colors.white,
+                                    fontSize: 17,
+                                  ),
+                                ),
+                              ),
+                            ],
+                          ),
+                        ),
+                      ),
+                    ),
+                  ],
+                ),
+              ),
+            ],
           ),
         ),
       ),

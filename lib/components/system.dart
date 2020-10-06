81ba027ae3d25fbb1997911fe933166df45b5675
Jappe02 <jasper.mannonen@gmail.com>
2020-10-06 19:02:27 +0300

    Edited system.dart

--- lib/components/system.dart
+++ lib/components/system.dart
@@ -2,3 +2,6 @@ import 'package:flutter/material.dart';
 import 'dart:async';
 
 const sstmSystemColorTheme = Color(0xFFf5f6fa); /// Default available color themes: light: 0xFFf5f6fa; dark: 0xFF222f3e; // The dafault color is light
+const sstmSystemDsktpBlurColor = Color(0xFF000000); /// Default available blur colors: light: 0xFFffffff; dark: 0xFF000000; // The dafault color is dark
+
+const String dsktpWallpaper = "lib/images/desktop/backgrounds/wallpaper2.jpg";

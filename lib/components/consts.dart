3c36c2bac15767b4ffb43b360ccd80ee0591fda8
Jappe02 <jasper.mannonen@gmail.com>
2021-04-08 15:53:14 +0300

    Changed the name to consts.

---
+++ lib/components/consts.dart
@@ -0,0 +1,23 @@
+import 'package:flutter/material.dart';
+
+/// --- [de-info]
+
+// name
+const String deNAME = "jappe-os desktop";
+
+// version
+const String deVERSION = "alpha: 1.1.3";
+
+/// --- [themes]
+
+// color theme
+const dsktpDesktopColorTheme = Color(0xFFf5f6fa); /// Default available color themes: light: 0xFFf5f6fa; dark: 0xFF222f3e; // The dafault color is light
+
+// blur color
+const dsktpDesktopDsktpBlurColor = Color(0xFF000000); /// Default available blur colors: light: 0xFFffffff; dark: 0xFF000000; // The dafault color is dark
+
+// desktop wallpaper
+const String dsktpWallpaper = "lib/images/desktop/backgrounds/wallpaper2.jpg";
+
+// primary/selected-color
+const dsktpDesktopPrimaryColor = Color(0xFF2196F3); /// Default: 0xFF2196F3
